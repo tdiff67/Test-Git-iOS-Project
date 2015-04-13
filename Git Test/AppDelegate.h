085ff555b4635646c2cc0acc6134ecf505570c23
Thomas Diffendal <thomasdiffendal@Thomass-MacBook-Pro-4.local>
2015-04-12 20:05:39 -0400

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Thomas Diffendal on 4/12/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Thomas Diffendal on 4/12/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
