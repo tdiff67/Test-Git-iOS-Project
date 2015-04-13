970a2388440d7e835962f92d3639db7e14c5e2a0
Thomas Diffendal <thomasdiffendal@Thomass-MacBook-Pro-4.local>
2015-04-12 20:34:50 -0400

    added a test class

---
+++ Git Test/TDTest.h
@@ -0,0 +1,13 @@
+//
+//  TDTest.h
+//  Git Test
+//
+//  Created by Thomas Diffendal on 4/12/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDTest : NSObject
+
+@end
---
+++ Git Test/TDTest.m
@@ -0,0 +1,13 @@
+//
+//  TDTest.m
+//  Git Test
+//
+//  Created by Thomas Diffendal on 4/12/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import "TDTest.h"
+
+@implementation TDTest
+
+@end
